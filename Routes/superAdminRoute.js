3c33c964a352077a6e4cb4c3d0c04a72fade1281
zahraaAed <zahraaalaaeddine94@gmail.com>
2025-02-28 21:21:12 +0200

    adding super admin and changing everything to commonjs

--- Routes/superAdminRoute.js
+++ Routes/superAdminRoute.js
@@ -7,7 +7,7 @@ const {
 
 const router = express.Router();
 
-/* router.post("/add/superadmin", createsuperAdmin); */
+router.post("/add/superadmin", createsuperAdmin);
 router.get('/superadmins', getAllSuperAdmins);
 router.get('/superadmin/:id', getSuperAdminById);
 
