435e0f9a2a6f65aa4e52951f8cd36c5fdf9793d6
Acil <acilhamieh4@gmail.com>
2025-03-03 01:49:47 +0200

    done for testing fixed income routes

--- Routes/fixedIncomeRoutes.js
+++ Routes/fixedIncomeRoutes.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const { addFixedIncome, getAllFixedIncomes, deleteFixedIncome } = require('../Controllers/fixedIncomeController');
 
 // Route to add a new fixed income
-router.post('/fixed-incomes', addFixedIncome);
+router.post('/addfixed-incomes', addFixedIncome);//done
 
 // Route to get all fixed incomes
-router.get('/fixed-incomes', getAllFixedIncomes);
+router.get('/fixed-incomes', getAllFixedIncomes);//done
 
 // Route to delete a fixed income by ID
-router.delete('/fixed-incomes/:id', deleteFixedIncome);
+router.delete('/fixed-incomes/:id', deleteFixedIncome);//done
 
 module.exports = router;
