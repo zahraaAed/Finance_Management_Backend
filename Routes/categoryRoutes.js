64eb6c68dca2947a7237a0369b06a01e69fc248a
Acil <acilhamieh4@gmail.com>
2025-03-03 01:37:48 +0200

    done for testing categories routes

--- Routes/categoryRoutes.js
+++ Routes/categoryRoutes.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const { addCategory, getAllCategories, deleteCategory } = require('../Controllers/categoryController');
 
 // Route to add a category
-router.post('/addcategorie', addCategory);
+router.post('/addcategorie', addCategory);//done
 
 // Route to get all categories
-router.get('/categories', getAllCategories);
+router.get('/categories', getAllCategories);//done
 
 // Route to delete a category by ID
-router.delete('/categories/:id', deleteCategory);
+router.delete('/categories/:id', deleteCategory);//done
 
 module.exports = router;
