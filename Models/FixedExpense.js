495160af9ddabfb16d22f39d46a2cecb46034bfd
Acil <acilhamieh4@gmail.com>
2025-03-02 12:55:02 +0200

    chanegs

--- Models/FixedExpense.js
+++ Models/FixedExpense.js
@@ -1,4 +1,4 @@
-const { DataTypes, Sequelize } = require('sequelize');
+const { DataTypes} = require('sequelize');
 const db = require('../config/dbconfig.js');
 const Category = require('./categoryModel');
 
